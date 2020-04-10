6d7a8deb40e5a7703bc982b0770d979cb26bad9b
EFREN RAYMUNDO MORALES <raymo99@comunidad.unam.mx>
2020-04-09 23:32:48 -0500

    a

---
+++ Proyecto/Cards/Cards.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+using AdaptiveCards.Rendering;
+using AdaptiveCards;
+
+namespace Proyecto.Cards
+{
+    public class Cards
+    {
+
+        //Aqui estan las funciones para crear las tarjetas
+        //Copien el primero, cambien el nombre de la funcion y la ruta
+        public static Attachment CreateAdaptiveCardAttachment()
+        {
+            // combine path for cross platform support
+            string[] paths = { ".", "Cards", "mintCard.json" }; //la ruta del archivo, solo cambiar el ultimo parametro por el nombre de tu archivo
+            var adaptiveCardJson = File.ReadAllText(Path.Combine(paths));
+
+            var adaptiveCardAttachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+            };
+            return adaptiveCardAttachment;
+        }
+
+        public static Attachment cafeIrlandesCard()
+        {
+            // combine path for cross platform support
+            string[] paths = { ".", "Cards", "cafeirlandesCard.json" };
+            var adaptiveCardJson = File.ReadAllText(Path.Combine(paths));
+
+            var adaptiveCardAttachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+            };
+            return adaptiveCardAttachment;
+        }
+
+        public static Attachment manhattanCard()
+        {
+            // combine path for cross platform support
+            string[] paths = { ".", "Cards", "manhattanCard.json" };
+            var adaptiveCardJson = File.ReadAllText(Path.Combine(paths));
+
+            var adaptiveCardAttachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+            };
+            return adaptiveCardAttachment;
+        }
+
+        public static Attachment lagunaAzulCard()
+        {
+            // combine path for cross platform support
+            string[] paths = { ".", "Cards", "lagunaAzulCard.json" };
+            var adaptiveCardJson = File.ReadAllText(Path.Combine(paths));
+
+            var adaptiveCardAttachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+            };
+            return adaptiveCardAttachment;
+        }
+
+        public static Attachment vodkaTonicCard()
+        {
+            // combine path for cross platform support
+            string[] paths = { ".", "Cards", "vodkaTonicCard.json" };
+            var adaptiveCardJson = File.ReadAllText(Path.Combine(paths));
+
+            var adaptiveCardAttachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+            };
+            return adaptiveCardAttachment;
+        }
+
+        public static Attachment coctelJulioCard()
+        {
+            // combine path for cross platform support
+            string[] paths = { ".", "Cards", "coctelJulioCard.json" };
+            var adaptiveCardJson = File.ReadAllText(Path.Combine(paths));
+
+            var adaptiveCardAttachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+            };
+            return adaptiveCardAttachment;
+        }
+    }
+}
---
+++ Proyecto/Dialogs/NormalDialog.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+
+namespace Proyecto.Dialogs
+{
+    public class NormalDialog : ComponentDialog
+    {
+        public NormalDialog() : base(nameof(NormalDialog))
+        {
+            AddDialog(new ChoicePrompt(nameof(ChoicePrompt))); //El Dialogo de las opciones
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[] //El dialogo en cascada 
+            {
+                //ChoiceCardStepAsync,
+                //ShowCardStepAsync,
+            }));
+
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        /*private async Task<DialogTurnResult> ChoiceCardStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+
+        }
+
+        private async Task<DialogTurnResult> ChoiceCardStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+
+        }*/
+    }
+}
